d6413c436de1bf8d84e8bc8390f024cc833f7ffb
Jairo Saul <JairoProDev@gmail.com>
2024-12-14 06:23:04 -0500

    Add initial configuration files for Next.js, PostCSS, Tailwind CSS, and TypeScript

---
+++ src/app/next-seo.config.ts
@@ -0,0 +1,18 @@
+const SEOConfig = {
+  titleTemplate: '%s | Buscadis',
+  defaultTitle: 'Buscadis',
+  description: 'Encuentra y publica adisos en menos de 1 minuto.',
+  openGraph: {
+    type: 'website',
+    locale: 'es_ES',
+    url: 'https://buscadis.com',
+    site_name: 'Buscadis',
+  },
+  twitter: {
+    handle: '@buscadis',
+    site: '@buscadis',
+    cardType: 'summary_large_image',
+  },
+};
+
+export default SEOConfig;
